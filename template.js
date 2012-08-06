8023540244cc615e063aacd030a052113dcbf6aa
Hiroshi Tashiro <infinith4@gmail.com>
2012-08-06 18:36:22 +0900

    Update template.js

--- template.js
+++ template.js
@@ -8,10 +8,18 @@ Template.prototype = {
     render: function(variables) {
         // この関数を実装してください
         var content =this.source;
-        
+        function htmlEscape(s){
+    	s=s.replace(/&/g,'&amp;');
+		s=s.replace(/>/g,'&gt;');
+		s=s.replace(/</g,'&lt;');
+		s=s.replace(/\"/,'&quot;');
+		s=s.replace(/\s/,'&nbsp;');
+		return s;
+	}
+	
         for(var n in variables){ 
         	var reg = new RegExp("{%\\s"+n+"\\s%}");
-        	content=content.replace(reg,variables[n]);
+        	content=content.replace(reg,htmlEscape(variables[n]));
         }
         return content;
     }
